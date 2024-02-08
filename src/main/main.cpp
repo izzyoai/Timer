482e7ec8306bde7d20488fc848949d941f1dc217
izzyoai <izzyoai@163.com>
2024-02-08 16:17:03 +0800

    更新一下CMake选择结构

---
+++ src/main/main.cpp
@@ -0,0 +1,25 @@
+﻿#include "timer/timer.h"
+
+using namespace std;
+
+int main() {
+	unique_ptr<Timer> timer = make_unique<Timer>();
+	uint64_t i = 0;
+	timer->AddTimer(5000, [&](const TimerNode& node) {
+		cout << "id:" << node.id << " expire:" << node.expire << " i:" << i << endl;
+		});
+	timer->AddTimer(3000, [&](const TimerNode& node) {
+		cout << "id:" << node.id << " expire:" << node.expire << " i:" << i << endl;
+		});	
+	timer->AddTimer(2000, [&](const TimerNode& node) {
+		cout << "id:" << node.id << " expire:" << node.expire << " i:" << i << endl;
+		});
+	timer->AddTimer(1000, [&](const TimerNode& node) {
+		cout << "id:" << node.id << " expire:" << node.expire << " i:" << i << endl;
+		});
+
+	while (true) {
+		i++;
+	}
+	return 0;
+}
